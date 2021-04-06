5ea858df6d795cc15b60bb05273d9834594841cf
nilgallego <47650435+withmeapp@users.noreply.github.com>
2021-04-06 21:18:11 +0200

    login feature

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,23 +1,15 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+const browserify = require("@cypress/browserify-preprocessor");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const resolve = require("resolve");
 
 module.exports = (on, config) => {
-  on("file:preprocessor", cucumber());
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: resolve.sync("typescript", { baseDir: config.projectRoot }),
+  };
+  options.browserifyOptions.plugin.unshift(["tsify"]);
+
+  on("file:preprocessor", cucumber(options));
 };
